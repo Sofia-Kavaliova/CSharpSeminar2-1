ea11df274cbe1ed33d48ba070e8f38b641ef643e
Sofia <kovaleva.sonia.02@mail.ru>
2023-10-12 20:38:32 +0300

    Add decision 001

---
+++ exercise001/Program.cs
@@ -0,0 +1,12 @@
+﻿// exercise 5
+// N -> zelie chisla ot -N do N
+
+System.Console.Write("Vvedite chislo: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int i = -number;
+
+while (i<= number)
+{
+    System.Console.Write(i+ "");
+    i++;
+}
