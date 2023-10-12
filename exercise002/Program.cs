a9e9a242acc7337ca29dec19e82494b1e148194c
Sofia <kovaleva.sonia.02@mail.ru>
2023-10-12 20:49:37 +0300

    add decisiun 002

---
+++ exercise002/Program.cs
@@ -0,0 +1,14 @@
+﻿// exercise001 
+// 3-x znachnoe chislo -> poslednia zifra
+
+System.Console.Write("Vvedite trehznachnoe chislo: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 999 || number < 100)
+{
+    System.Console.WriteLine("Vi vveli ne trehznachnoe chislo!");
+}
+else
+{
+    System.Console.WriteLine(number%10);
+}
