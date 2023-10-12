e3e050278a8d67809fcfca47a9aa066ffbcceb15
Sofia <kovaleva.sonia.02@mail.ru>
2023-10-12 21:01:49 +0300

    add decision 003

---
+++ exercise003/Program.cs
@@ -0,0 +1,10 @@
+﻿// exercise 11
+// Sluchainoetrehznachnoe chislo, udalit vtoruu zifru
+
+int randomNumber = new Random().Next(100,1000);
+System.Console.WriteLine(randomNumber);
+int leftNumber = randomNumber/100;
+int rightNumber = randomNumber%10;
+
+int res = leftNumber * 10 + rightNumber;
+System.Console.WriteLine(res);
