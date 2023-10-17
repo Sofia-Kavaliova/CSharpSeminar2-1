8719edb8ff5595fd8ebc3c33192fd8d57a096346
Sofia <kovaleva.sonia.02@mail.ru>
2023-10-17 11:56:01 +0300

    Add decision Task004

---
+++ exercise004/Program.cs
@@ -0,0 +1,15 @@
+﻿// exercise 13
+
+System.Console.Write("vvedite pervoe chislo: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Vvedite vtoroe chislo: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+
+if (firstValue % secondValue ==0)
+{
+    System.Console.WriteLine("Kratno");
+}
+else
+{
+    System.Console.WriteLine("Ne kratno, ostatok "+firstValue % secondValue);
+}
