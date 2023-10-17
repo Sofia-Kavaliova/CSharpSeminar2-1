c511b6b66b9dbc229ea40c754d8af5644d2c229c
Sofia <kovaleva.sonia.02@mail.ru>
2023-10-17 13:42:43 +0300

    add decision task006

---
+++ exercise006/Program.cs
@@ -0,0 +1,15 @@
+﻿// exercise 17
+
+Console.Write("Vvedite pervoe chislo: ");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Vvedite vtoroe chislo: ");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+
+if (firstValue == secondValue * secondValue || secondValue == firstValue * firstValue)
+{
+    System.Console.WriteLine("Da");
+}
+else
+{
+    System.Console.WriteLine("Net");
+}
